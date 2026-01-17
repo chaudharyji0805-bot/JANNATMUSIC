12966c3c7a960feb9eeaf91225ffa534934cffd1
BOTZEMPIRE <chaudharyji0805@gmail.com>
2026-01-17 11:08:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,6 +42,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
+     "https://github.com/chaudharyji0805-bot/JANNATMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
