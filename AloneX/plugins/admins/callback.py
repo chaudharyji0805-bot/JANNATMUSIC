4a0777cfb9522d88443b824a01a91bc3d38ebe0b
BOTZEMPIRE <chaudharyji0805@gmail.com>
2026-02-10 13:40:03 +0530

    Update callback.py

--- AloneX/plugins/admins/callback.py
+++ AloneX/plugins/admins/callback.py
@@ -4,7 +4,7 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from AloneX import YouTube, app
-from AloneX.core.call import Hotty
+from AloneX.core.call import Alone
 from AloneX.misc import SUDOERS, SPECIAL_ID, db
 from AloneX.utils.database import (
     get_active_chats,
@@ -395,4 +395,5 @@ async def markup_timer():
 
 
 asyncio.create_task(markup_timer())
+
 
